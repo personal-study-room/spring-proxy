72839d9f9e30cf52085859bac4e4d20950958687
hongsh429 <shh0429@kakao.com>
2024-07-25 09:22:22 +0900

    스프링이 제공하는 빈 후처리기 - 1
    - 이미 빈 후처리기를 만들어 놨다. 우리는 advisor를 등록하기만하면 알아서 적용된다.
    - MethodInterceptor를 구현한 advice를 만들고 이를 통해 어드바이져를 빈으로 등록해서 사용하는 경우
    
    스프링이 제공하는 빈 후처리기 -2
    - AspectJExpressionPointcut : 실무에선 이것만 사용

--- src/main/java/com/example/proxy/config/v6_autoproxy/AutoProxyConfig.java
+++ src/main/java/com/example/proxy/config/v6_autoproxy/AutoProxyConfig.java
@@ -5,6 +5,7 @@ import com.example.proxy.config.AppV2Config;
 import com.example.proxy.config.v4_proxyfactory.advisce.LogTraceAdvice;
 import com.example.proxy.trace.logtrace.LogTrace;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -15,7 +16,7 @@ import org.springframework.context.annotation.Import;
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
 
-  @Bean
+//  @Bean
   public Advisor advisor1(LogTrace logTrace) {
     // pointcut
     NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
@@ -26,4 +27,31 @@ public class AutoProxyConfig {
 
     return new DefaultPointcutAdvisor(pointcut, advice);
   }
+
+//  @Bean
+  public Advisor advisor2(LogTrace logTrace) {
+    // pointcut
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    pointcut.setExpression("execution(* com.example.proxy.app..*(..))");
+
+    // advice
+    LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+
+    //advisor = pointcut + advice
+    return new DefaultPointcutAdvisor(pointcut, advice);
+  }
+
+  @Bean
+  public Advisor advisor3(LogTrace logTrace) {
+    // pointcut
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    pointcut.setExpression("execution(* com.example.proxy.app..*(..)) && !execution(* com.example.proxy.app..noLog(..))");
+
+    // advice
+    LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+
+    //advisor = pointcut + advice
+    return new DefaultPointcutAdvisor(pointcut, advice);
+  }
+
 }
